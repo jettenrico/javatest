9746a21eba692fd1a8b243818eb9e16580321a2a
Jett <jericochandra4@gmail.com>
2022-11-08 10:21:31 +0700

    add class

---
+++ javafundamental/src/main/java/javafundamental/TrainingDay2.java
@@ -0,0 +1,14 @@
+package javafundamental;
+
+/*
+@Author Jett A.K.A. Jett Enrico Chandra
+CT0
+Created with Eclipse Version
+Created on 08-11-2022 10:19:38
+Last Modified on 08-11-2022 10:19:38
+Version 1.0
+*/
+
+public class TrainingDay2 {
+
+}
