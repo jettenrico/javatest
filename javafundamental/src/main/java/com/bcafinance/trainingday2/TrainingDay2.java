6aff4a3132425eaaebd89adb666e880863e673a6
Jett <jericochandra4@gmail.com>
2022-11-08 15:39:33 +0700

    training day 2

--- javafundamental/src/main/java/com/bcafinance/trainingday2/TrainingDay2.java
+++ javafundamental/src/main/java/com/bcafinance/trainingday2/TrainingDay2.java
@@ -1,5 +1,7 @@
 package com.bcafinance.trainingday2;
 
+import java.util.Random;
+
 /*
 @Author Jett A.K.A. Jett Enrico Chandra
 CT0
@@ -10,5 +12,71 @@ Version 1.0
 */
 
 public class TrainingDay2 {
-
+	public static void main(String[] args) {
+		char abc ='1';
+		String input = "Tb@14XzM!@#i^";
+		char[] asciInput = input.toCharArray();
+		int total =0;
+		int def = abc;
+		int myVal = 35;
+		int test = 0;
+		int test1 =0;
+		boolean reminderOn = true;
+		Random ran = new Random();
+		int intAsciChar=0;
+		String tes = "";
+		
+		System.out.println(abc);
+		System.out.println(def);
+//		
+		for (int a = 0; a<input.length(); a++) {
+			if ((asciInput[a] >= 65 && asciInput[a] <= 90) ||
+					(asciInput[a] >= 97 && asciInput[a] <= 122) ||
+					(asciInput[a] >= 48 && asciInput[a] <= 57)) {
+				total += asciInput[a];	
+			}
+		}
+		
+		System.out.println(total);
+		
+		for (int i = 0; i < 9; i++) {
+			if (i<3) {
+				intAsciChar = ran.nextInt(65,91); 
+				tes += (char)intAsciChar;
+			}
+			else if (i<6) {
+				intAsciChar = ran.nextInt(97,122); 
+				tes += (char)intAsciChar;
+			}
+			else  {
+				intAsciChar = ran.nextInt(48,57); 
+				tes += (char)intAsciChar;
+			}
+			
+		}
+		
+		System.out.println(tes);
+		
+		
+//		if ( myVal > 100 ) 
+//			{  if (reminderOn) {
+//				myVal = myVal % 100;
+//			}
+//			else {
+//				myVal = myVal / 100;
+//			}
+//			}
+//		else
+//		{
+//		System.out.print(myVal);
+//		}
+//		
+		for (int i = 0; i< 10; i++) {
+			test++;
+			for(int j =0; j < 20; j++) {
+				test++;
+			}
+		}
+		System.out.println(test);
+	}
 }
